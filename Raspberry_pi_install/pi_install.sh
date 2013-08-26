494f02c2d26ed37e32921d5dd5366ffa048a9b43
bwattendorf <bwattendorf@gmail.com>
2013-08-26 16:39:47 -0400

    made some changes to install script

--- Raspberry_pi_install/pi_install.sh
+++ Raspberry_pi_install/pi_install.sh
@@ -3,40 +3,36 @@
 
 clear
 
+cd~
+
+
 #first lets update and upgrade the pi
 sudo apt-get update -qy
 
 sudo apt-get upgrade -qy
 
 
-#git is ridiculously important to us
-
-sudo apt-get install -qy git-core
-
 
 #next to install openvnc
 
 sudo apt-get install tightvncserver -qy
 
-
-
 #now to make it so vnc auto starts 
 
 sudo mkdir /home/pi/.config/autostart
 
-sudo cp $instdir/server/config/tightvnc.desktop /home/pi/.config/autostart/tightvnc.desktop
+
+#NOTE need to fix this and add $instdir
+#sudo cp $instdir/server/config/tightvnc.desktop #/home/pi/.config/autostart/tightvnc.desktop
 
 
-#setup DNSDynamic
 
-sudo apt-get install ddclient -qy
 
-sudo cp $instdir/server/config/ddclient.conf /etc/
-
+#now to install OctoPrint
 
 cd ~
 sudo apt-get install python-pip python-dev git -qy
-git clone https://github.com/foosel/OctoPrint.git
+#git clone https://github.com/foosel/OctoPrint.git
 cd OctoPrint
 sudo pip install -r requirements.txt
 
@@ -48,31 +44,28 @@ sudo pip uninstall flask werkzeug
 sudo pip install flask==0.9 werkzeug==0.8.3
 
 
-cd ~/OctoPrint/
-git pull
-sudo pip install -r requirements.txt
 
+#install mjpg-streamer 
+#for web cam support
 
 cd ~
 sudo apt-get install subversion libjpeg8-dev imagemagick libav-tools -qy
 
 svn co https://mjpg-streamer.svn.sourceforge.net/svnroot/mjpg-streamer mjpg-streamer
 
-
 cd mjpg-streamer/mjpg-streamer
 
 make
 
+
+
 # now to install haproxy
 
-
-
-
 sudo apt-get install haproxy -qy
-
 
 sudo apt-get update && sudo apt-get install avahi-daemon -qy
 
 
 
+#now to clean up apt-get to make more space on the SD card
 sudo apt-get clean
